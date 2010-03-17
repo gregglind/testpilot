6aba523ca49ea9423733a9dd428a42585979f42e
jonathandicarlo@jonathan-dicarlos-macbook-pro.local <none@none>
2010-03-16 17:12:28 -0700

    More work on bug 542107: refactored creation of xul elements, added links to tasks' pages, thank-you message for submitted studies.

--- extension/content/all-studies-window.js
+++ extension/content/all-studies-window.js
@@ -1,95 +1,137 @@
 
-function xulSubmitButtonHandler(experimentId) {
-  dump("You clicked the XUL submit button!\n");
+// TODO make sure this works when there are a lot of items (i.e. does scroll
+// bar appear?)
 
-}
+var TestPilotXulWindow = {
+  onSubmitButton: function(experimentID) {
+    // TODO implement
+    dump("You clicked the XUL submit button for study id " + experimentID + "!\n");
+  },
 
-function onLoadAllStudiesWindow() {
-  Components.utils.import("resource://testpilot/modules/setup.js");
-  Components.utils.import("resource://testpilot/modules/tasks.js");
+  addXulLink: function (container, text, url) {
+    let link = document.createElement("label");
+    link.setAttribute("value", text);
+    link.setAttribute("class", "text-link");
+    link.setAttribute("onclick",
+      "if (event.button==0) {TestPilotXulWindow.openURL('" + url + "');}");
+    container.appendChild(link);
+  },
 
-  var currStudiesRowset = document.getElementById("current-studies-rowset");
-  var finishedStudiesRowset = document.getElementById("finished-studies-rowset");
-  var upcomingStudiesRowset = document.getElementById("upcoming-studies-rowset");
+  addLabel: function(container, text, styleClass) {
+    let label = document.createElement("label");
+    label.setAttribute("value", text);
+    if (styleClass) {
+      label.setAttribute("class", styleClass);
+    }
+    container.appendChild(label);
+  },
 
-  let experiments = TestPilotSetup.getAllTasks();
-
-  for (let i = 0; i < experiments.length; i++) {
-    let task = experiments[i];
-    let newRow = document.createElement("row");
+  addImg: function(container, src) {
     let newImg = document.createElement("image");
-    newImg.setAttribute("src",
-      "chrome://testpilot/skin/new_study_48x48.png");
-    newRow.appendChild(newImg);
-    let newLabel = document.createElement("label");
-    newLabel.setAttribute("value", task.title);
-    newLabel.setAttribute("class", "pilot-title");
-    newRow.appendChild(newLabel);
+    newImg.setAttribute("src", "chrome://testpilot/skin/" + src);
+    container.appendChild(newImg);
+  },
 
-    if (task.status == TaskConstants.STATUS_FINISHED) {
-      let vbox = document.createElement("vbox");
-      let endDateLabel = document.createElement("label");
-      endDateLabel.setAttribute("value", "Finished on " +
-                                  (new Date(task.endDate)).toDateString());
-      vbox.appendChild(endDateLabel);
+  openURL: function(url) {
+    let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                        .getService(Components.interfaces.nsIWindowMediator);
+    let win = wm.getMostRecentWindow("navigator:browser");
+    var browser = win.getBrowser();
+    var tab = browser.addTab(url);
+    browser.selectedTab = tab;
+  },
 
-      let submitButton = document.createElement("button");
-      submitButton.setAttribute("label", "Submit");
-      submitButton.setAttribute("oncommand",
-                                "xulSubmitButtonHandler(" + task.id + ");");
-      vbox.appendChild(submitButton);
-      newRow.appendChild(vbox);
+  onLoad: function () {
+    Components.utils.import("resource://testpilot/modules/setup.js");
+    Components.utils.import("resource://testpilot/modules/tasks.js");
+
+    let experiments = TestPilotSetup.getAllTasks();
+
+    for (let i = 0; i < experiments.length; i++) {
+      let task = experiments[i];
+      let newRow = document.createElement("row");
+
+      this.addImg(newRow, "new_study_48x48.png");
+
+      let textVbox = document.createElement("vbox");
+      newRow.appendChild(textVbox);
+      this.addLabel(textVbox, task.title, "pilot-title");
+      this.addXulLink(textVbox, "More Info", task.defaultUrl);
+
+
+      // Create the rightmost status area, depending on status:
+      let statusVbox = document.createElement("vbox");
+      if (task.status == TaskConstants.STATUS_FINISHED) {
+        this.addLabel( statusVbox, "Finished on " +
+                                   (new Date(task.endDate)).toDateString());
+
+        let submitButton = document.createElement("button");
+        submitButton.setAttribute("label", "Submit");
+        submitButton.setAttribute("oncommand",
+          "TestPilotXulWindow.onSubmitButton(" + task.id + ");");
+        statusVbox.appendChild(submitButton);
+      }
+      if (task.status == TaskConstants.STATUS_IN_PROGRESS ||
+          task.status == TaskConstants.STATUS_STARTING ) {
+
+        this.addLabel(statusVbox, "Started on " +
+                                 (new Date(task.startDate)).toDateString());
+
+       /* let progressBar = document.createElement();
+        vbox.appendChild(progressBar);*/
+
+        this.addLabel(statusVbox, "Will end on " +
+                                 (new Date(task.endDate)).toDateString());
+      }
+      if (task.status == TaskConstants.STATUS_SUBMITTED ||
+          task.status == TaskConstants.STATUS_RESULTS ||
+          task.status == TaskConstants.STATUS_ARCHIVED) {
+
+        this.addImg(statusVbox, "ready_submit_48x48.png");
+        this.addLabel(statusVbox, "Thank you for submitting!");
+      }
+
+      newRow.appendChild(statusVbox);
+
+      // Use status to decide which panel to add this to:
+      let rowset;
+      switch (task.status) {
+      case TaskConstants.STATUS_NEW:
+      case TaskConstants.STATUS_PENDING:
+        rowset = document.getElementById("upcoming-studies-rowset");
+      break;
+      case TaskConstants.STATUS_STARTING:
+      case TaskConstants.STATUS_IN_PROGRESS:
+      case TaskConstants.STATUS_FINISHED:
+        rowset = document.getElementById("current-studies-rowset");
+      break;
+      case TaskConstants.STATUS_SUBMITTED:
+      case TaskConstants.STATUS_RESULTS:
+      case TaskConstants.STATUS_ARCHIVED:
+      case TaskConstants.STATUS_CANCELLED:
+        rowset = document.getElementById("finished-studies-rowset");
+      break;
+      }
+      rowset.appendChild(newRow);
     }
-
-    if (task.status == TaskConstants.IN_PROGRESS ||
-        task.status == TaskConstants.STARTING ) {
-      let vbox = document.createElement("vbox");
-      newRow.appendChild(vbox);
-
-      let startDateLabel = document.createElement("label");
-      startDateLabel.setAttribute("value", "Started on " +
-                                  (new Date(task.startDate)).toDateString());
-      vbox.appendChild(startDateLabel);
-
-     /* let progressBar = document.createElement();
-      vbox.appendChild(progressBar);*/
-
-      let endDateLabel = document.createElement("label");
-      endDateLabel.setAttribute("value", "Will end on " +
-                                  (new Date(task.endDate)).toDateString());
-      vbox.appendChild(endDateLabel);
-    }
-
-    let rowset;
-    switch (task.status) {
-    case TaskConstants.STATUS_NEW:
-    case TaskConstants.STATUS_PENDING:
-      rowset = document.getElementById("upcoming-studies-rowset");
-    break;
-    case TaskConstants.STATUS_STARTING:
-    case TaskConstants.STATUS_IN_PROGRESS:
-    case TaskConstants.STATUS_FINISHED:
-      rowset = document.getElementById("current-studies-rowset");
-    break;
-    case TaskConstants.STATUS_SUBMITTED:
-    case TaskConstants.STATUS_RESULTS:
-    case TaskConstants.STATUS_ARCHIVED:
-    case TaskConstants.STATUS_CANCELLED:
-      rowset = document.getElementById("finished-studies-rowset");
-    break;
-    }
-    rowset.appendChild(newRow);
-
+  }
+}
 
     // Show links for:
    // task.currentStatusUrl
     // task.infoPageUrl
     // task.resultsUrl // may not exist
     // task.defaultUrl points to one of these!
+    // Links in add-ons manager are just
+    // <label class="text-link" onclick="if (event.button ==0) {
+    // openURL(this.getAttribute('homepageURL'));}"
+    // homepageURL = "http://whatever"/>
+    // styles for text-link are defined in skin/classic/global/console.css
+    // and skin/classic/global/global.css
+    //
+    // (I think we can just link to href="chrome://global/skin/" type="text/css"
     //if (experiments[i].taskType == TaskConstants.TYPE_EXPERIMENT)
 
-  }
-}
 
   // If there are no experiments here, it must be because we're
   // not done loading yet... try again in a few seconds.

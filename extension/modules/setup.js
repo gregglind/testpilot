b73bec74beba7ea579df06d57eebd3b69691b1c5
Jono X <jono@mozilla.com>
2010-06-30 08:08:29 -0700

    Removed debugging dump() statement accidentally left in

--- extension/modules/setup.js
+++ extension/modules/setup.js
@@ -570,7 +570,6 @@ let TestPilotSetup = {
 	      task.defaultUrl, false, function() {
                 /* on close callback (Bug 575767) -- when the "new study
                  * starting" popup is dismissed, then the study can start. */
-                dump("You closed notification, changing study status!\n");
                 task.changeStatus(TaskConstants.STATUS_IN_PROGRESS, true);
                 TestPilotSetup.reloadRemoteExperiments();
               });
